66a6a2b32729c0637396a01e1043ef71e2ea9611
Joerg Schoeneburg <schoeneburg.j@googlemail.com>
2023-03-13 14:41:54 +0100

    Update _1_joomla.js

--- assets/js/components/_1_joomla.js
+++ assets/js/components/_1_joomla.js
@@ -2,7 +2,9 @@
 
 (function () {
     var item = document.querySelector('.com-content-article__body');
-    if (item.children.length == 0) {
-        item.parentNode.parentNode.remove();
+    if (item) {
+        if (item.children.length == 0) {
+            item.parentNode.parentNode.remove();
+        }
     }
 })();
